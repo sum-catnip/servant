82f6aeedcc6a9c9d1625c4c576eca5923068c252
Catnip <catnip@catnip.fyi>
2019-01-22 19:44:11 +0100

    handled web exception on start and added host parameter

--- src/main.cpp
+++ src/main.cpp
@@ -3,12 +3,19 @@
 #include "servant.h"
 
 int main(int argc, char** argv) {
+    std::wstring host_address(U("http://*:6969"));
+    if(argc > 1) host_address = conversions::to_string_t(std::string(argv[1]));
     logger::log(logger::level::DEBUG, "waddup pimps");
 
-    servant* s = new servant("modules", U("http://*:6969"));
-    s->start();
-
-    getchar();
-
-    delete(s);
+    try {
+        servant* s = new servant("modules", host_address);
+        s->start();
+        getchar();
+        delete(s);
+    }
+    catch(const web::http::http_exception& ex) {
+        logger::log(logger::level::ERROR, 
+            std::string("failed to start webservice. details: ") + ex.what()
+        );
+    }
 }
